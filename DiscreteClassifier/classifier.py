2462bda06292278f7015e2b34539a33ccc721d5c
Brian <dathamor@gmail.com>
2025-06-16 16:23:19 -0600

    completed modular classifier for backend
    Co-authored-by: Max Ma <maxma233@users.noreply.github.com>

--- DiscreteClassifier/classifier.py
+++ DiscreteClassifier/classifier.py
@@ -1,61 +1,100 @@
-# %%
 import pandas as pd
 import numpy as np
 from collections import Counter as ctr
 
-# %%
-data = pd.read_csv('timelymealsdiscreteannotated.csv', skiprows=1, names=['type','text', 'c3', 'c4', 'c5'], delimiter=',', encoding='ISO-8859-1')
+import os
+# import subprocess
 
-# %%
-data['clean'] = data.text.apply(lambda x: x.lower().split())
+# Verifying and moving to correct location
+os.chdir(os.getcwd() + '\\DiscreteClassifier')
+print(f'Current Working Directory: {os.getcwd()}')
+# subprocess.run("dir", shell=True)
 
-# %%
-data.drop(['c3', 'c4', 'c5'], axis=1, inplace=True)
+def read_file() -> pd.DataFrame:
+    """ 
+        Building the data from the .csv file by
+        parsing it into columns and then normalizing
+        by tokenizing the input. 
+        Returns: Pandas Dataframe containing the data
+    """
+    data = pd.read_csv('timelymealsdiscreteannotated.csv', skiprows=1, names=['type','text', 'c3', 'c4', 'c5'], delimiter=',', encoding='ISO-8859-1')
 
-# %%
-type_ctr = ctr(data.type)
+    # Tokenizing the input
+    data['clean'] = data.text.apply(lambda x: x.lower().split())
+    data.drop(['c3', 'c4', 'c5'], axis=1, inplace=True)
 
-type_ctr['Invalid']/len(data), type_ctr['Valid']/len(data)
+    return data
 
-# %%
-test = data.sample(frac=0.1)
-train = data[~data.index.isin(test.index)]
+def build_classifier_dict(data: pd.DataFrame) -> dict:
+    """
+        Builds a dictionary that contains the respective
+        token counter for each type of response (Valid/Invalid)
+        Returns: Dictionary containing counter entries of the valid and invalid tokens
+    """
+    words_valid_invalid = {}
 
-# %%
-counter = ctr(train.type)
+    # For each type (Valid/Invalid) have a counter that accounts for all tokenized entries for each respective response
+    # Kinda acts like a guide to show you what words most likely appear in a valid or invalid response
+    for col_name in list(set(data.type)):
+        sub_df = data[data.type == col_name]
+        words_valid_invalid[col_name] = []
+        for row in sub_df.clean:
+            for word in row:
+                words_valid_invalid[col_name].append(word)
+        words_valid_invalid[col_name] = ctr(words_valid_invalid[col_name])
 
-def Pa(X=''):
-    return counter[X] / len(train)
+    return words_valid_invalid
 
-# %%
-words_ctr = ctr([word for row in train.clean for word in row])
+class Classifier:
+    def __init__(self):
+        """ 
+            Creates a discrete classifier from a provided dataset
+            using helper functions. The purpose is to indicate whether
+            a provided prompt is valid or invalid for our application.
+        """
+        self.data = read_file()   
+        self.valid_invalid_dict = build_classifier_dict(self.data)
 
-def Pb(W=''):
-    if W not in words_ctr: return 0.000001
-    return words_ctr[W] / sum(words_ctr.values())
+        # Saved information for probability functions
+        self.type_ctr = ctr(self.data.type)
+        self.words_ctr = ctr([word for row in self.data.clean for word in row])
 
-# %%
-words_valid_invalid = {}
+    def verify(self, prompt: str) -> int:
+        """
+            Main function: Verifies the prompt and returns a bool value on case
+            Return: 1 for valid, 0 for invalid
+        """
+        tokenized_prompt = []
+        tokenized_prompt.extend(prompt.lower().split())
 
-for col_name in list(set(data.type)):
-    sub_df = train[train.type == col_name] #masking data type to only be ham sub-datatype
-    words_valid_invalid[col_name] = []
-    for row in sub_df.clean:
-        for word in row:
-            words_valid_invalid[col_name].append(word)
-    words_valid_invalid[col_name] = ctr(words_valid_invalid[col_name])
+        valid_case = self.Ps(tokenized_prompt, X='Valid')
+        invalid_case = self.Ps(tokenized_prompt, X='Invalid')
 
-def Pba(W='', X=''):
-    t = words_valid_invalid[X]
-    if W not in t: return 0.0000001
-    return t[W] / sum(t.values())
+        return 1 if valid_case > invalid_case else 0
 
-# %%
-def Pab(X='', W=''):
-    return Pba(W, X) * Pa(X) / Pb(W)
+    # Probability Helper functions   
+    
+    def Pa(self, X=''):
+        return self.type_ctr[X] / len(self.data)
 
-# %%
-def Ps(T, X=''):
-    return np.prod([Pab(X=X, W=word) for word in T])
+    def Pb(self, W='') -> float:
+        if W not in self.words_ctr: return 0.000001
+        return self.words_ctr[W] / sum(self.words_ctr.values())
+        
+    def Pba(self, W='', X='') -> float:
+        t = self.valid_invalid_dict[X]
+        if W not in t: return 0.0000001
+        return t[W] / sum(t.values())
 
-
+    def Pab(self, X='', W='') -> float:
+        return self.Pba(W, X) * self.Pa(X) / self.Pb(W)
+    
+    def Ps(self, T, X=''):
+        return np.prod([self.Pab(X=X, W=word) for word in T])
+    
+if __name__ == "__main__":
+    classifier = Classifier()
+    result = classifier.verify(prompt="Where is the zoo")
+    
+    print(result)
+    print('Done!')
