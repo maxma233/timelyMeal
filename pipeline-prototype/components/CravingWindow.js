e8140948e4040211af9b7023def7cfe074e1afcc
Brian <dathamor@gmail.com>
2025-07-13 17:22:30 -0600

    list animation fixed
    Co-authored-by: Max Ma <maxma233@users.noreply.github.com>
    Co-authored-by: Chris Tudela <ctudel@users.noreply.github.com>

--- pipeline-prototype/components/CravingWindow.js
+++ pipeline-prototype/components/CravingWindow.js
@@ -1,27 +1,31 @@
-
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useContext, createContext } from 'react';
 import { View, StyleSheet, TextInput, Button, Pressable, Image, Animated, Text } from 'react-native';
 import { ShoppingBasket03Icon, ListViewIcon, AddToListIcon } from 'hugeicons-react';
-// import { LocateFixed } from 'lucide-react-native';
 import { List } from '../assets/images.js'
 import ListTransition from '../components/ListTransition.js'
 
+export const ElementContext = createContext(null);
 
 function CravingWindow({ preferences, preferenceSetter, locationVal, locationSetter }) {
 
     const [craving, SetCraving] = useState('');
     const [shoppingCart, setShoppingCart] = useState({ dishes: [], restaurants: [] });
-    const [showCart, setShowCart] = useState(false);
+    const [toggleCart, setToggleCart] = useState(false);
+    const [componentLoaded, setComponentLoaded] = useState(false);
     const animatedValue = useRef(new Animated.Value(0)).current;
 
     const [listHeight, setListHeight] = useState(0);
     const ref = useRef(null);
 
     useEffect(() => {
+
+        setComponentLoaded(true);
+
         if (ref.current) {
+            console.log("Component has loaded!");
             setListHeight(ref.current.clientHeight);
         }
-    }, [showCart]);
+    }, []);
 
     // Makes sure when you back up down the list of questions 
     // it makes sure the showCart toggle is false so the list 
@@ -32,8 +36,6 @@ function CravingWindow({ preferences, preferenceSetter, locationVal, locationSet
         }
     }, [locationVal])
 
-
-
     if (locationVal !== 4) {
         return;
     }
@@ -43,7 +45,7 @@ function CravingWindow({ preferences, preferenceSetter, locationVal, locationSet
     }
 
     const toggleShoppingCart = (e) => {
-        setShowCart(!showCart);
+        setToggleCart(!toggleCart);
         console.log("Opening the list!");
     }
 
@@ -75,9 +77,25 @@ function CravingWindow({ preferences, preferenceSetter, locationVal, locationSet
 
             </View>
 
-            {showCart &&
+            {/* {showCart &&
+            } */}
+
+            <View style={{ zIndex: -1, position: 'absolute', width: 300, height: 450, opacity: 0 }}
+                ref={ref}>
+                <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>
+                    Items
+                </Text>
+            </View>
+
+            <ElementContext value={{
+                listHeight,
+                setListHeight,
+                componentLoaded,
+                toggleCart
+            }}>
+
                 <ListTransition
-                    listHeight={listHeight}
+                    // listHeight={listHeight}
                     style={{
                         position: 'fixed',
                         bottom: 0,
@@ -86,19 +104,17 @@ function CravingWindow({ preferences, preferenceSetter, locationVal, locationSet
                         borderRadius: 24,
                     }}>
                     <View style={{ width: 300, height: 450 }}>
-                        <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>
-                            Items
-                        </Text>
+                        <Pressable
+                            onPress={toggleShoppingCart}
+                        >
+                            <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>
+                                Items
+                            </Text>
+                        </Pressable>
                     </View>
                 </ListTransition>
-            }
 
-            <View style={{ zIndex: -1, position: 'fixed', width: 300, height: 450, opacity: 0 }}
-                ref={ref}>
-                <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>
-                    Items
-                </Text>
-            </View>
+            </ElementContext>
 
 
         </View>
--- pipeline-prototype/components/ListTransition.js
+++ pipeline-prototype/components/ListTransition.js
@@ -1,38 +1,54 @@
-import React, { useEffect, useRef } from 'react';
+import React, { createContext, useRef, useContext, useEffect, useState } from 'react';
 import { Animated, Text, View } from 'react-native';
+import { ElementContext } from './CravingWindow.js';
 
-const ListTransition = props => {
+const DEFAULT_START_Y = 50;
+const DEFAULT_ANIMATION_TIMING = 2000;
 
-    // const animatedValue = useRef(new Animated.Value(0)).current;
-    // const currentX = useRef(new Animated.Value(0)).current;
-    // const endX = 150;
+function ListTransition(props) {
+    const parentContext = useContext(ElementContext);
+    const currentY = useRef(new Animated.Value(0)).current;
 
-    const listTransitionHeight = -150 - props.listHeight;
-    console.log(listTransitionHeight);
-
-    const currentY = useRef(new Animated.Value(listTransitionHeight)).current;
+    const listHeightOffset = -parentContext.listHeight + DEFAULT_START_Y;
+    const startPosition = listHeightOffset;
     const endY = 0;
 
-    console.log(currentY);
-    console.log(props);
-
     useEffect(() => {
-        Animated.timing(currentY, {
-            toValue: endY,
-            duration: 2000,
-            useNativeDriver: true,
-        }).start();
-    }, [currentY]);
+
+        const showAnimate = () => {
+            Animated.timing(currentY, {
+                toValue: startPosition,
+                duration: DEFAULT_ANIMATION_TIMING,
+                useNativeDriver: true,
+            }).start();
+        }
+
+        const hideAnimate = () => {
+            Animated.timing(currentY, {
+                toValue: endY,
+                duration: DEFAULT_ANIMATION_TIMING,
+                useNativeDriver: true
+            }).start();
+        }
+
+        if (!parentContext.componentLoaded) {
+            return;
+        }
+
+        parentContext.toggleCart ? showAnimate() : hideAnimate();
+
+    }, [parentContext.toggleCart]);
 
     return (
         <Animated.View // Special animatable View
             style={{
                 ...props.style,
                 // left: currentX,
-                bottom: currentY,
+                bottom: startPosition,
+                transform: [{ translateY: currentY }],
             }}>
             {props.children}
-        </Animated.View>
+        </Animated.View >
     );
 };
 
