62e3d3f729fa2baa566141492bf63de3acacf0f4
Brian <dathamor@gmail.com>
2025-07-20 18:24:27 -0600

    updated script
    Co-authored-by: Max Ma <maxma233@users.noreply.github.com>
    Co-authored-by: Chris Tudela <ctudel@users.noreply.github.com>

--- culinaryBERT/training/unique_ctr.py
+++ culinaryBERT/training/unique_ctr.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
     file = open(file='./train_original.txt', mode='r')
     duplicates = 0
+    duplicate_examples = []
 
     # All the unique stuff in the file
     unique_examples = []
@@ -8,46 +9,79 @@ if __name__ == "__main__":
 
     positive_examples = 0
     negative_examples = 0
+    borked = 0
     is_positive = False
+    skip = False
+
+    line_num = 0
 
     while (True):
         curr_line = file.readline()
+        line_num += 1
 
         # Done with the file
         if (curr_line == ''):
             break
 
-        if (curr_line != '\n'):
-            add_this: list[str] = curr_line.split()
-            add_this[0] = str.lower(add_this[0])
-
-            if not is_positive and add_this[1].startswith(('B-', 'I-')):
-                is_positive = True
-
-            new_line = " ".join(add_this)
-            curr_example += new_line + '\n'
-            continue
-            
         try:
-            # If does not through ValueError it is in the list
+            if (curr_line != '\n'):
+                # Lowercase the example on the current line
+                add_this: list[str] = curr_line.split()
+                add_this[0] = str.lower(add_this[0])
+
+                # Positive examples only contain non 'O' tags
+                if not is_positive and add_this[1].startswith(('B-', 'I-')):
+                    is_positive = True
+
+                new_line = " ".join(add_this)
+                
+                # print("Current new line: ", new_line)
+
+                # Producing safe output
+                bytes_line = new_line.encode(encoding='ascii', errors='strict')
+                safe_line = bytes_line.decode(encoding='utf-8', errors='strict')
+
+                print("safe line: ", safe_line)
+
+                curr_example += safe_line + '\n'
+                continue
+        except UnicodeEncodeError:
+            # print('borked data found, skipping!')
+            skip = True
+
+        try:
+            
+            # Skip the borked example
+            if (skip):
+                borked += 1
+                raise Exception
+
+            # If does not throw ValueError it is in the list
             unique_examples.index(curr_example)
             duplicates+=1
-            print('It is a duplicate')
+            # print(f'Curr example on line ({line_num}): ', curr_example)
+            duplicate_examples.append(curr_example)
+            # print('It is a duplicate')
         except ValueError:
-            print('Unique one found!')
+            # print('Unique one found!')
             positive_examples += 1 if is_positive else 0
             negative_examples += 1 if not is_positive else 0
             unique_examples.append(curr_example)
+        except Exception:
+            # print('Skipping example')
+            pass
         finally:
             # Reset the example
             curr_example = ''
             is_positive = False
+            skip = False
 
-    print('Unique set: ', unique_examples)
+    # print('Unique set: ', unique_examples)
     print('Number of unique elements: ', len(unique_examples))
-    print('Duplicates: ', duplicates)    
+    print(f'Duplicates ({duplicates}): ', duplicate_examples)    
     print('Positive examples: ', positive_examples)
     print('Negative examples: ', negative_examples)
+    print('Borked: ', borked)
 
     try:
         with open('./unique_stuff.txt', 'w') as unique_file:
