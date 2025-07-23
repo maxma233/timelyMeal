99845a12c2bd519603d513f3a6e015868bb99722
Brian <dathamor@gmail.com>
2025-07-22 19:33:58 -0600

    added culinaryBERT
    Co-authored-by: Max Ma <maxma233@users.noreply.github.com>
    Co-authored-by: Chris Tudela <ctudel@users.noreply.github.com>

--- pipeline-prototype/backend/server.py
+++ pipeline-prototype/backend/server.py
@@ -2,13 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import time as t
 
-# Used for the Gemma 3 (Fine-Tuned) model
+# Used for the LMs
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, DistilBertForTokenClassification, DistilBertTokenizerFast
 import gc
 from huggingface_hub import login
-from transformers import pipeline
 
 from dotenv import load_dotenv
 
@@ -21,9 +19,11 @@ import os
 app = Flask(__name__)
 CORS(app) # enable CORS to allow request from native react app
 
+# NOTE: Change structure later
 current_dir = Path(__file__).resolve().parent
 # Add the path correctly
 script_dir = current_dir.parent.parent / 'DiscreteClassifier'
+culinaryBERT_filepath = '../../culinaryBERT/models/culinaryBERT'
 DEFAULT_FILE_LOCATION = script_dir / 'timelymealsdiscreteannotated.csv'
 
 if script_dir.exists():
@@ -41,47 +41,53 @@ classifier = Classifier(file_location=DEFAULT_FILE_LOCATION)
 # Use CUDA for the model
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# Loading the model
+# Loading the models and tokenizers
 model_name = "timely/TimelyAI"
 secret_token = os.getenv("SECRET_KEY")
+
+culinaryBERT_tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-cased")
+culinaryBERT_model = DistilBertForTokenClassification.from_pretrained(culinaryBERT_filepath)
+culinaryBERT_model.to(device)
 
 # Login
 login(token=secret_token)
 
-try:
-    # Load tokenizer first
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_name,
-        trust_remote_code=True
-    )
-    print("Tokenizer loaded successfully")
+# try:
+#     # Load tokenizer first
+#     tokenizer = AutoTokenizer.from_pretrained(
+#         model_name,
+#         trust_remote_code=True
+#     )
+#     print("Tokenizer loaded successfully")
     
-    # Load model with multi-GPU support
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name
-    )
-    print("Model loaded successfully!")
+#     # Load model with multi-GPU support
+#     model = AutoModelForCausalLM.from_pretrained(
+#         model_name
+#     )
+#     print("Model loaded successfully!")
 
-except Exception as e:
-    print(f"Error loading model: {e}")
-    print("Trying without quantization...")
+# except Exception as e:
+#     print(f"Error loading model: {e}")
+#     print("Trying without quantization...")
     
-    # Fallback: Load without quantization
-    try:
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            device_map="auto",
-            torch_dtype=torch.float16,
-            trust_remote_code=True,
-            low_cpu_mem_usage=True
-        )
-        print("Model loaded without quantization")
-    except Exception as e2:
-        print(f"Failed to load model: {e2}")
-        model = None
+#     # Fallback: Load without quantization
+#     try:
+#         model = AutoModelForCausalLM.from_pretrained(
+#             model_name,
+#             device_map="auto",
+#             torch_dtype=torch.float16,
+#             trust_remote_code=True,
+#             low_cpu_mem_usage=True
+#         )
+#         print("Model loaded without quantization")
+#     except Exception as e2:
+#         print(f"Failed to load model: {e2}")
+#         model = None
 
 # Setting up pipeline
-pipe = pipeline("text-generation", model="timely/TimelyAI", device="cpu", torch_dtype=torch.bfloat16)
+gemma_pipe = pipeline("text-generation", model="timely/TimelyAI", device="cpu", torch_dtype=torch.bfloat16)
+ 
+culinaryBERT_pipe = pipeline("ner", model=culinaryBERT_model, tokenizer=culinaryBERT_tokenizer, device="cpu")
 
 @app.route('/add', methods=['POST'])
 def append_to_classifier():
@@ -157,7 +163,7 @@ def prompt_model():
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "Also, can you add the genre of food the dish is before you state the meal in parentheses (Ex: (Italian) Pasta Primavera)? Can you also list the quantity per dish in []? Choose decisive meals. Ex: Turkey Club as opposed to any sandwich."}
+                {"type": "text", "text": "Also, can you add the ethnic cuisine of food the dish is before you state the meal in parentheses (Ex: (American) Hamburger)? Can you also list the quantity per dish inside square brackets (Ex: [1 burger])? Choose takeout meals. Choose decisive meals. Ex: Turkey Club as opposed to any sandwich. Do not add quips."}
             ]
         }
     ]
@@ -179,7 +185,7 @@ def prompt_model():
     # print(f'Start time: {t.perf_counter() - start_time} seconds')
     
     # Call the model
-    output = pipe(text_inputs=base_messages, max_new_tokens=500)
+    output = gemma_pipe(text_inputs=base_messages, max_new_tokens=750)
     
     model_response = output[0]["generated_text"][-1]["content"]
 
