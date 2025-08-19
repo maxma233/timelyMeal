c7fd917f4efc063e120c4528e24101d017a5db26
Brian <dathamor@gmail.com>
2025-08-18 21:53:21 -0600

    refactored questionnairewindow
    Co-authored-by: Max Ma <maxma233@users.noreply.github.com>
    Co-authored-by: Chris Tudela <ctudel@users.noreply.github.com>

--- pipeline-prototype/components/QuestionnaireWindow.js
+++ pipeline-prototype/components/QuestionnaireWindow.js
@@ -1,83 +1,86 @@
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext, useEffect, useRef } from "react";
 import { View, Text } from "react-native";
-import QuestionnaireMap from "./QuestionnaireMap";
 import FoodTypeSelector from "./FoodTypeSelector";
 import DurationSelector from "./DurationSelector";
 import MealSelector from "./MealSelector"
 import PreferenceWindow from "./PreferenceWindow";
 import CravingWindow from "./CravingWindow";
-import BackButton from "./BackButton";
-import { PageIndicator } from 'react-native-page-indicator';
+import NavButton from "./NavButton";
+import { PageIndicator } from './PageIndicator/PageIndicator';
 import ScheduleSelector from "./ScheduleSelector";
+import QuestionBlock from "./QuestionBlock";
+import DataChecker from './DataChecker';
 
 export const LocationContext = createContext();
+export const QuestionnaireContext = createContext();
+export const BeadThemeContext = createContext();
 
-const DEFAULT_QUESTIONNAIRE = ['How would you like to eat?',
-    'How long do you need the plan to be?',
-    'How many meals per day?',
-    'Preferences?',
-    'What are you craving?',
+export const DEFAULT_QUESTIONNAIRE = [
+    [
+        { question: 'Type of Food:', component: <FoodTypeSelector />, checkFor: ["foodType"] }, 
+        { question: 'Duration (Days)?', component: <DurationSelector />, checkFor: ["duration"] },
+        { question: 'Meals per day?', component: <MealSelector />, checkFor: ["numMeals"] },
+    ],
+    [
+        { question: 'Preferences?', component: <PreferenceWindow />, checkFor: ["preferences", "ethnicCuisines"] }
+    ],
+    [
+        { question: 'What are you craving?', component: <CravingWindow />, checkFor: [["preferences", "dishes"], ["preferences", "restaurants"]] }
+    ],
     // 'What is the budget?',
-    'Do you need help scheduling?'];
+    // 'Do you need help scheduling?'
+];
+
+const DEFAULT_QUESTIONNAIRE_DATA_STATE = {
+        foodType: '', 
+        duration: 0, 
+        numMeals: 0, 
+        preferences: { ethnicCuisines: [], dishes: [], restaurants: [] },
+    };
 
 function QuestionnaireWindow() {
-    const [locationVal, setLocationVal] = useState(4);
-    const [foodType, setFoodType] = useState('');
-    const [duration, setDuration] = useState(0);
-    const [numMeals, setNumMeals] = useState(0);
-    const [preferences, setPreferences] = useState({ ethnicCuisines: [], dishes: [], restaurants: [] });
-    const [budget, setBudget] = useState(0);
-    const [schedulingFlag, setSchedulingFlag] = useState(false);
-    const [scheduleAhead, setScheduleAhead] = useState(null);
+    
+    const [locationVal, setLocationVal] = useState(0);
+    const [questionnaireData, setQuestionnaireData] = useState(DEFAULT_QUESTIONNAIRE_DATA_STATE);
+    const [indicatorBeads, setIndicatorBeads] = useState(new Array(DEFAULT_QUESTIONNAIRE.length).fill("#333"));
 
-    // const renderedQuestions = () => {
+    useEffect(() => {
+        console.log("Questionnaire Data", questionnaireData)
 
-    //     switch (questionNum) {
-    //         case (0):
-    //             return <FoodTypeSelector foodTypeSetter={setFoodType} />
-    //         case (1):
-    //             return <DurationSelector durationSetter={setDuration} />
 
-    //     }
-    // }
+
+
+
+
+
+    }, [questionnaireData]);
 
     return (
         <View style={{ width: '75vw' }}>
-
-
-            <QuestionnaireMap locationVal={locationVal} />
-
             <View>
 
                 <LocationContext value={{ locationVal, setLocationVal }}>
+                    <QuestionnaireContext value={{ questionnaireData, setQuestionnaireData }}>
+                        <BeadThemeContext value={{ indicatorBeads, setIndicatorBeads }}>
+                            <DataChecker questions={DEFAULT_QUESTIONNAIRE[locationVal]}/>
+                            <View style={{ alignSelf: 'center', display: 'flex', position: 'relative', flexDirection: 'row', gap: '20px' }}>
+                                <View style={{width: 'fit-content', position: 'absolute', left: '-5rem', top: 0, display: 'flex', flexDirection: 'row'}}>
+                                </View>
 
-                    <div style={{ display: 'flex' }}>
-                        <BackButton />
-                        <Text style={{ fontSize: '2.3rem' }}>
-                            {DEFAULT_QUESTIONNAIRE[locationVal]}
-                        </Text>
-                    </div>
+                                    <View style={{ display: 'flex', flexDirection: 'col', gap: '20px', margin: 'auto', width: '100%', justifyContent: 'space-evenly' }}>
+                                        <View style={{ display: 'flex', flexDirection: 'row', gap: '10px', justifyContent: 'center' }}>
+                                            <NavButton direction="left"/>
+                                                <PageIndicator style={{ }} vertical={false} count={DEFAULT_QUESTIONNAIRE.length} current={locationVal} beadReferences={indicatorBeads} />
+                                            <NavButton direction="right"/>
+                                        </View>
+                                        <QuestionBlock questions={DEFAULT_QUESTIONNAIRE[locationVal]} />
+                                    </View>
 
-
-                    <View style={{ alignSelf: 'flex-start', left: 0, display: 'flex', flexDirection: 'row', gap: '20px' }}>
-                        <PageIndicator style={{ alignSelf: 'center' }} vertical={true} count={DEFAULT_QUESTIONNAIRE.length} current={locationVal} />
-
-                        <View style={{ display: 'flex', flexDirection: 'row', gap: '20px', margin: 'auto', width: '100%', justifyContent: 'space-evenly' }}>
-                            <FoodTypeSelector foodTypeSetter={setFoodType} locationVal={locationVal} locationSetter={setLocationVal} />
-                            <DurationSelector durationVal={duration} durationSetter={setDuration} locationVal={locationVal} locationSetter={setLocationVal} />
-                            <MealSelector mealSetter={setNumMeals} locationVal={locationVal} locationSetter={setLocationVal} />
-                            <PreferenceWindow preferences={preferences} preferenceSetter={setPreferences} locationVal={locationVal} locationSetter={setLocationVal} />
-                            <CravingWindow preferences={preferences} preferenceSetter={setPreferences} />
-                            <ScheduleSelector scheduleAhead={scheduleAhead} setScheduleAhead={setScheduleAhead} />
-                        </View>
-                    </View>
-
+                            </View>
+                        </BeadThemeContext>
+                    </QuestionnaireContext>
                 </LocationContext>
-
-
             </View>
-
-
         </View >
     );
 }
