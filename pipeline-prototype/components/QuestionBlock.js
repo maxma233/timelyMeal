04f5db82477c24ebe955b5ffc586d741b33e391b
Brian <dathamor@gmail.com>
2025-09-07 22:21:43 -0600

    refactored logic to show multiple questions attached to one question object

--- pipeline-prototype/components/QuestionBlock.js
+++ pipeline-prototype/components/QuestionBlock.js
@@ -6,8 +6,24 @@ function QuestionBlock({ questions }) {
     return (
             <View>
                 {questions.map((element, index) => (
-                    <View key={index} style={{display: 'flex' , gap: '1rem', marginBottom: '30px'}}> 
-                        <Text style={{fontSize: '3vw'}}>{element.question}</Text>
+                    <View key={index} style={{display: 'flex', flexDirection: 'column', gap: '1rem', marginBottom: '30px'}}> 
+                        <View
+                            style={{ display: 'flex', flexDirection: 'row', gap: '.5rem' }}
+                        >
+                            {element['questions'].map((question, index) => {
+                                return (
+                                    <View
+                                    key={index}
+                                    >
+                                        <Text
+                                            style={{fontSize: '3vw'}}
+                                            >
+                                            {question}
+                                        </Text>
+                                    </View>
+                                );
+                            })}
+                        </View>
                         <View style={{ display: 'flex', flexDirection: 'row', gap: '1rem', fontSize: '1.5vw', width: '100%', alignSelf: 'flex-start'}}>{element.component}</View>
                     </View>
                 ))}    
